1587c55130e4952088474901c5e436c2bf00a79f
Andrew Rembrandt <andrew@rembrandt.me.uk>
2018-05-03 17:29:51 +0100

    Initial

---
+++ import.sql
@@ -0,0 +1,28 @@
+CREATE TABLE security_raw(
+  SecId integer,
+  SecMnem char(20),
+  ProductSupType char(6),
+  Product char(12),
+  CUSIP char(9),
+  Agency char(3),
+  Prefix char(2),
+  PrefixId integer,
+  IssAmt double,
+  CollType char(4),
+  IssDt date,
+  MatDt date,
+  OrigWac double,
+  OrigWam integer,
+  OrigWala integer,
+  RetireDt date,
+  IssueId integer,
+  PriceId integer,
+  TBAEligCode char(3),
+  SsnCode char(3),
+  InsSrc char(4),
+  InsDt char(17),
+  UpdSrc char(4),
+  UpdDt char(17));
+
+LOAD DATA LOCAL INPATH('GNM_SEC.DAT') OVERWRITE INTO TABLE security_raw;
+
---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+schematool -dbType derby -initSchema
+
